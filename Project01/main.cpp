51345a9b081fef109d4a00610b68db10ea7e4ee5
TEJ-416 <tsingh_be21@thapar.edu>
2024-01-24 16:34:24 +0530

    Initial Commit

---
+++ Project01/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Project01
+//
+//  Created by Tejvir Singh on 24/01/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
